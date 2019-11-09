3f1d088bf78ff49fa64c2bec9ac310f6b28b9360
고병학 <gobyeonghag@gobyeonghag-ui-MacBookPro.local>
2019-11-09 18:45:30 +0900

    edit MyTable

--- src/components/MyTable.js
+++ src/components/MyTable.js
@@ -4,14 +4,38 @@ import domtoimage from 'dom-to-image';
 class MyTable extends React.Component {
     
     render(){
+        let tableStyle={
+            
+        }
+
         return(
             <>
                 <h1>내 시간표</h1>
-                <div>{this.props.contents["session1"]["titleTop"]}</div>
-                <div>{this.props.contents["session2"]["titleTop"]}</div>
-                <div>{this.props.contents["session3"]["titleTop"]}</div>
-                <div>{this.props.contents["session4"]["titleTop"]}</div>
-                <div>{this.props.contents["session5"]["titleTop"]}</div>
+                <div>
+                    <span>{this.props.contents["session1"]["speakerCate"]}</span>
+                    &nbsp;
+                    {this.props.contents["session1"]["titleTop"]}
+                </div>
+                <div>
+                    <span>{this.props.contents["session2"]["speakerCate"]}</span>
+                    &nbsp;
+                    {this.props.contents["session2"]["titleTop"]}
+                </div>
+                <div>
+                    <span>{this.props.contents["session3"]["speakerCate"]}</span>
+                    &nbsp;
+                    {this.props.contents["session3"]["titleTop"]}
+                </div>
+                <div>
+                    <span>{this.props.contents["session4"]["speakerCate"]}</span>
+                    &nbsp;
+                    {this.props.contents["session4"]["titleTop"]}
+                </div>
+                <div>
+                    <span>{this.props.contents["session5"]["speakerCate"]}</span>
+                    &nbsp;
+                    {this.props.contents["session5"]["titleTop"]}
+                </div>
             </>
         );
     }
